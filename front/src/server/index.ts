66b7546622551a84560a63b8e4edde14f749fac5
Allan Gabriel <gabrielisaway@gmail.com>
2025-04-28 22:25:17 -0300

    Delete front directory

--- front/src/server/index.ts
+++
@@ -1,8 +0,0 @@
-// index.ts ou app.ts
-import { server } from './server';
-
-const port = 3000; // Defina a porta em que o servidor vai escutar
-
-server.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
