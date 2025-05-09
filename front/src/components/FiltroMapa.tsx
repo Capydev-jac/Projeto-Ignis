95820789e235ac53892b5f72037c9aec93dd84f4
Allan Gabriel <gabrielisaway@gmail.com>
2025-05-09 19:09:52 -0300

    Update FiltroMapa.tsx

--- front/src/components/FiltroMapa.tsx
+++ front/src/components/FiltroMapa.tsx
@@ -115,48 +115,49 @@ const FiltroMapa: React.FC<FiltroMapaProps> = ({ onFiltrar }) => {
           }}
         />
 
-        {/* 🔥 Selecionar Estado */}
-        <label htmlFor="estado">Estados</label>
-        <Select id="estado" value={estado} onChange={(e) => setEstado(e.target.value)}>
+        {/* Selecionar Estado */}
+        <label htmlFor="id_estado">Estados</label>
+        <Select id="id_estado" value={estado} onChange={(e) => setEstado(e.target.value)}>
           <option value="">Selecione um estado</option>
-          <option value="ac">Acre</option>
-          <option value="al">Alagoas</option>
-          <option value="ap">Amapá</option>
-          <option value="am">Amazonas</option>
-          <option value="ba">Bahia</option>
-          <option value="ce">Ceará</option>
-          <option value="es">Espírito Santo</option>
-          <option value="go">Goiás</option>
-          <option value="ma">Maranhão</option>
-          <option value="mt">Mato Grosso</option>
-          <option value="ms">Mato Grosso do Sul</option>
-          <option value="mg">Minas Gerais</option>
-          <option value="pa">Pará</option>
-          <option value="pb">Paraíba</option>
-          <option value="pr">Paraná</option>
-          <option value="pe">Pernambuco</option>
-          <option value="pi">Piauí</option>
-          <option value="rj">Rio de Janeiro</option>
-          <option value="rn">Rio Grande do Norte</option>
-          <option value="rs">Rio Grande do Sul</option>
-          <option value="ro">Rondônia</option>
-          <option value="rr">Roraima</option>
-          <option value="sc">Santa Catarina</option>
-          <option value="sp">São Paulo</option>
-          <option value="se">Sergipe</option>
-          <option value="to">Tocantins</option>
-          <option value="df">Distrito Federal</option>
+          <option value="12">Acre</option>
+          <option value="27">Alagoas</option>
+          <option value="16">Amapá</option>
+          <option value="13">Amazonas</option>
+          <option value="29">Bahia</option>
+          <option value="23">Ceará</option>
+          <option value="32">Espírito Santo</option>
+          <option value="52">Goiás</option>
+          <option value="21">Maranhão</option>
+          <option value="51">Mato Grosso</option>
+          <option value="50">Mato Grosso do Sul</option>
+          <option value="31">Minas Gerais</option>
+          <option value="15">Pará</option>
+          <option value="25">Paraíba</option>
+          <option value="41">Paraná</option>
+          <option value="26">Pernambuco</option>
+          <option value="22">Piauí</option>
+          <option value="33">Rio de Janeiro</option>
+          <option value="24">Rio Grande do Norte</option>
+          <option value="43">Rio Grande do Sul</option>
+          <option value="11">Rondônia</option>
+          <option value="14">Roraima</option>
+          <option value="42">Santa Catarina</option>
+          <option value="35">São Paulo</option>
+          <option value="28">Sergipe</option>
+          <option value="17">Tocantins</option>
+          <option value="53">Distrito Federal</option>
         </Select>
 
-        {/* 🔥 Selecionar Bioma */}
+        {/* Selecionar Bioma */}
         <label htmlFor="bioma">Biomas</label>
         <Select id="bioma" value={bioma} onChange={(e) => setBioma(e.target.value)}>
           <option value="">Selecione um bioma</option>
-          <option value="cerrado">Cerrado</option>
-          <option value="caatinga">Caatinga</option>
-          <option value="pantanal">Pantanal</option>
-          <option value="mata Atlântica">Mata Atlântica</option>
-          <option value="pampa">Pampa</option>
+          <option value="3">Cerrado</option>
+          <option value="2">Caatinga</option>
+          <option value="6">Pantanal</option>
+          <option value="4">Mata Atlântica</option>
+          <option value="5">Pampa</option>
+          <option value="1">Amazônia</option>
         </Select>
 
         {/* 🔥 Selecionar Datas */}
