842fd5e15aef3fd65c33f9463afdbf4d1dc00b24
Felipe Ribeiro <felipe_ribeiro_286@hotmail.com>
2025-06-13 20:06:56 -0300

    Update OcorrenciaRoutes.ts

--- Projeto-Ignis/backend/src/routes/OcorrenciaRoutes.ts
+++ Projeto-Ignis/backend/src/routes/OcorrenciaRoutes.ts
@@ -9,6 +9,8 @@ router.get('/datas_disponiveis', OcorrenciaController.DatasDisponiveis);
 router.get("/risco", OcorrenciaController.Filtrar_risco_fogo);
 router.get("/foco_calor", OcorrenciaController.Filtrar_foco_calor);
 router.get("/area_queimada", OcorrenciaController.Filtrar_area_queimada);
+router.post("/inserir-foco", OcorrenciaController.InserirFocoCalor);
+router.get("/relatorio-focos", OcorrenciaController.GerarRelatorioFocos);
 
 // 📊 ROTAS DE GRÁFICO
 router.get("/grafico/area_queimada", OcorrenciaController.GraficoAreaQueimada);
